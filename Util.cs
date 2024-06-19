13167d6b6763e007044a5a29213496b31f586f4d
Josh-Manley <joshmanley45@gmail.com>
2024-06-18 22:29:08 -0600

    added make badges

--- Util.cs
+++ Util.cs
@@ -1,5 +1,8 @@
 using System;
+using System.IO;
 using System.Collections.Generic;
+using System.Net.Http;
+using SkiaSharp;
 
 namespace CatWorx.BadgeMaker
 {
@@ -15,7 +18,26 @@ namespace CatWorx.BadgeMaker
     }
     public static void MakeCSV(List<Employee> employees)
     {
-
+      if (!Directory.Exists("data"))
+      {
+        Directory.CreateDirectory("data");
+      }
+      using (StreamWriter file = new StreamWriter("data/employees.csv"))
+      {
+        file.WriteLine("ID,Name,PhotoUrl");
+        for (int i = 0; i < employees.Count; i++)
+        {
+          string template = "{0},{1},{2}";
+          file.WriteLine(string.Format(template, employees[i].GetId(), employees[i].GetFullName(), employees[i].GetPhotoUrl()));
+        }
+      }
     }
+    public static void MakeBadges(List<Employee> employees)
+    // {
+    //   SKImage newImage = SKImage.FromEncodedData(File.OpenRead("badge.png"));
+
+    //   SKData data = newImage.Encode();
+    //   data.SaveTo(File.OpenWrite("data/employeeBadge.png"));
+    // }
   }
 }
